f2135490511fb7d38ed2c5c1f9f111956bae8d17
JuanA <juanandreslujan30@gmail.com>
2021-10-21 23:05:02 -0500

    Cuadro de busqueda y cards para visualizacion responsive

--- src/pages/admin/Vehiculos.jsx
+++ src/pages/admin/Vehiculos.jsx
@@ -1,6 +1,8 @@
 import { nanoid } from 'nanoid';
 import React, { useEffect, useState, useRef } from 'react'
 import { ToastContainer, toast } from 'react-toastify';
+import { Tooltip } from '@mui/material';
+import Dialog from '@mui/material/Dialog';
 import 'react-toastify/dist/ReactToastify.css';
 
 const vehiculosBackend = [
@@ -83,37 +85,72 @@ const Vehiculos = () => {
 };
 
 const TablaVehiculos = ({ listaVehiculos }) => {
+    const [busqueda, setBusqueda] = useState('');
+    const [vehiculosFiltrados, setVehiculosFiltrados] = useState(listaVehiculos);
+
+    useEffect(() => {
+        console.log('Busqueda', busqueda);
+        console.log('Lista original', listaVehiculos);
+        setVehiculosFiltrados(
+            listaVehiculos.filter(
+                (elemento) => {
+                    return JSON.stringify(elemento).toLocaleLowerCase().includes(busqueda.toLocaleLowerCase());
+                })
+        );
+    }, [busqueda, listaVehiculos])
+
     return (
         <div className='flex flex-col items-center w-full justify-center'>
+            <input
+                value={busqueda}
+                onChange={(e) => setBusqueda(e.target.value)}
+                placeholder='buscar'
+                className='border-2 border-gray-900 px-3 py-1 self-start rounded-md focus:outline-none focus:border-indigo-500' />
             <h2 className='text-2xl font-extrabold text-gray-800'>Todos los vehículos</h2>
-            <table className='tabla'>
-                <thead>
-                    <tr>
-                        <th>Nombre del vehículo</th>
-                        <th>Marca del vehículo</th>
-                        <th>Modelo del vehículo</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {listaVehiculos.map((vehiculo) => {
-                        return (
-                            <FilaVehiculo key={nanoid()} vehiculo={vehiculo} />
-                        );
-                    })}
-                </tbody>
-            </table>
+            <div className='hidden md:flex w-full'>
+                <table className='tabla'>
+                    <thead>
+                        <tr>
+                            <th>Nombre del vehículo</th>
+                            <th>Marca del vehículo</th>
+                            <th>Modelo del vehículo</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {vehiculosFiltrados.map((vehiculo) => {
+                            return (
+                                <FilaVehiculo
+                                    key={nanoid()}
+                                    vehiculo={vehiculo} />
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
+            <div className=' flex flex-col w-full m-2 md:hidden'>
+                {vehiculosFiltrados.map((el) => {
+                    return (
+                    <div className='bg-gray-500 m-2 shadow-xl flex flex-col rounded-xl p-2'>
+                        <span>{el.nombre}</span>
+                        <span>{el.marca}</span>
+                        <span>{el.modelo}</span>
+                    </div>
+                );
+                })}
+            </div>
         </div>
     );
-}
+};
 
 
 const FilaVehiculo = ({ vehiculo }) => {
+    const [openDialog, setOpenDialog] = useState(false);
     const [edit, setEdit] = useState(false);
     const [infoNuevoVehiculo, setInfoNuevoVehiculo] = useState({
         nombre: vehiculo.nombre,
         marca: vehiculo.marca,
-        modelo : vehiculo.modelo,
+        modelo: vehiculo.modelo,
 
     });
     const actualizarVehiculo = () => {
@@ -122,7 +159,7 @@ const FilaVehiculo = ({ vehiculo }) => {
     }
 
     const eliminarVehiculo = () => {
-        
+        setOpenDialog(false);
     }
     return (
         <tr>
@@ -130,25 +167,25 @@ const FilaVehiculo = ({ vehiculo }) => {
                 <>
                     <td>
                         <input className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
-                        type="text" 
-                        defaultValue={infoNuevoVehiculo.nombre}
-                         onChange = {(e)=> 
-                            setInfoNuevoVehiculo({...infoNuevoVehiculo, nombre: e.target.value})}
-                         />
-                    </td>
-                    <td>
-                        <input className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
-                        type="text" defaultValue={infoNuevoVehiculo.marca} 
-                        onChange = {(e)=> 
-                            setInfoNuevoVehiculo({...infoNuevoVehiculo, marca: e.target.value})}
+                            type="text"
+                            defaultValue={infoNuevoVehiculo.nombre}
+                            onChange={(e) =>
+                                setInfoNuevoVehiculo({ ...infoNuevoVehiculo, nombre: e.target.value })}
                         />
                     </td>
                     <td>
                         <input className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
-                        type="text" 
-                        defaultValue={infoNuevoVehiculo.modelo}
-                        onChange = {(e)=> 
-                            setInfoNuevoVehiculo({...infoNuevoVehiculo, modelo: e.target.value})}
+                            type="text" defaultValue={infoNuevoVehiculo.marca}
+                            onChange={(e) =>
+                                setInfoNuevoVehiculo({ ...infoNuevoVehiculo, marca: e.target.value })}
+                        />
+                    </td>
+                    <td>
+                        <input className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
+                            type="text"
+                            defaultValue={infoNuevoVehiculo.modelo}
+                            onChange={(e) =>
+                                setInfoNuevoVehiculo({ ...infoNuevoVehiculo, modelo: e.target.value })}
                         />
                     </td>
                 </>
@@ -162,17 +199,50 @@ const FilaVehiculo = ({ vehiculo }) => {
             <td>
                 <div className='flex w-full justify-around'>
                     {edit ?
-                    <i onClick={() => actualizarVehiculo()}  
-                    className="fas fa-check text-green-600 hover:text-green-400"></i>
-                   :
+                        <>
+                            <Tooltip title='Confirmar edición' arrow>
+                                <i onClick={() => actualizarVehiculo()}
+                                    className="fas fa-check text-green-600 hover:text-green-400" />
+                            </Tooltip>
+                            <Tooltip title='Cancelar edición' arrow>
+                                <i onClick={() => setEdit(!edit)}
+                                    className="fas fa-ban hover:text-yellow-500 text-yellow-700" />
 
-                    <i onClick={() => setEdit(!edit)} 
-                    className="fas fa-pencil-alt hover:text-yellow-500 text-yellow-700"></i>}
+                            </Tooltip>
+                        </>
+                        :
+                        <>
+                            <Tooltip title='Editar vehículo' arrow>
+                                <i onClick={() => setEdit(!edit)}
+                                    className="fas fa-pencil-alt hover:text-yellow-500 text-yellow-700" />
 
-                    <i 
-                    onClick= {()=> eliminarVehiculo()}
-                    className="fas fa-trash text-red-700 hover:text-red-500"></i>
+                            </Tooltip>
+
+                            <Tooltip title='Eliminar vehículo' arrow>
+                                <i onClick={() => setOpenDialog(true)}
+                                    className="fas fa-trash text-red-700 hover:text-red-500" />
+                            </Tooltip>
+                        </>
+                    }
+
+
                 </div>
+                <Dialog open={openDialog}>
+                    <div className='p-8 flex flex-col'>
+                        <h1 className='text-gray-900 text-2xl font-bold'>¿Está seguro de querer eliminar este vehículo? </h1>
+                        <div className='flex w-full items-center justify-center my-4'>
+                            <button
+                                onClick={() => eliminarVehiculo()}
+                                className=' mx-2 px-4 py-2 bg-green-500 text-white hover:bg-green-800 rounded-lg shadow-md'>
+                                Sí</button>
+                            <button
+                                onClick={() => setOpenDialog(false)}
+                                className=' mx-2 px-4 py-2 bg-red-600 text-white hover:bg-red-900 rounded-lg shadow-md'>
+                                No</button>
+                        </div>
+                    </div>
+                </Dialog>
+
             </td>
         </tr>
     )
@@ -207,7 +277,7 @@ const FormularioCreacionVehiculos = ({ setVehiculos, listaVehiculos, setMostrarT
                 <label className='flex flex-col' htmlFor='nombre'>
                     Nombre del vehículo
                     <input
-                     name='nombre'
+                        name='nombre'
                         required
                         className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
                         type="text"
@@ -231,7 +301,7 @@ const FormularioCreacionVehiculos = ({ setVehiculos, listaVehiculos, setMostrarT
                 <label className='flex flex-col' htmlFor='modelo'>
                     Modelo del vehículo
                     <input
-                     name='nombre'
+                        name='nombre'
                         className='bg-gray-50 border border-gray-600 p-2 rounded-lg m-2'
                         type="number"
                         required
